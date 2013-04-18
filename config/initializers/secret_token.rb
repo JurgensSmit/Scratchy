15ea98994944a7d6205a4eb8cee5a9ee784e153c
Jurgens Smit <contact@jurgenssmit.com>
2013-04-19 09:41:09 +1000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scratchy::Application.config.secret_token = 'fe23ab51bb52ba00cc9f0c20b80d55fbfd3520d1070a6482b7c2733bf06c2834f7f06a48295d3364f0128b93e61bd2bcac0c4a6b13927a4ba5b9da65efc52e74'
